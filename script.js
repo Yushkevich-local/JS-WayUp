529ca23fc5510c34858bba156c7757b5e8c6cbaa
Kiryl <yushkevich.kiryl@gmail.com>
2021-08-07 21:48:44 +0200

    JS-lesson 2

--- script.js
+++ script.js
@@ -1,40 +1,62 @@
 'use strict';
 
-let city = {
-  name: 'Brest',
-  country: 'Belarus',
-  number: 300000,
-  footbal: true 
-};
 
-let a = 40,
-    b = 70;
-console.log(a * b);
-
-let time = 2,
-    speedOfFirst = 95,
-    speedOfSecond = 114;
-console.log((speedOfFirst + speedOfSecond) * time);
-
-
-const randomNumber = Math.floor(Math.random() * 100);
-// ELSE IF METHOD
-// if (randomNumber < 20) {
-//   console.log('randomNumber меньше 20');
-// } else if (randomNumber > 50) {
-//   console.log('randomNumber больше 50');
-// } else {
-//   console.log('randomNumber больше 20, и меньше 50');
-// }
-
-switch (true) {
-  case randomNumber <= 20 :
-    console.log('randomNumber меньше 20');
-    break;
-  case randomNumber >= 50 :
-    console.log('randomNumber больше 50');
-    break;
-  default :
-    console.log('randomNumber от 20 до 50');
-    break;
+//Задание 1
+for (let i = 10; i <= 50; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
 }
+
+
+//Задание 2
+const meObject = {
+  firstName: 'Kiryl',
+  lastName: 'Yushkeich',
+  age: 19,
+  animal: true
+}
+
+
+//Задание 3
+const array = [
+  'я в Симбирск,',
+  'в трактире.',
+  'с утра',
+  'В ту же ночь',
+  'Я остановился',
+  'для закупки', 
+  'что и было поручено Савельичу.',
+  'приехал,',
+  'где должен был',
+  'нужных вещей',
+  'отправился по лавкам',
+  'пробыть сутки',
+  'Савельич'
+]
+
+array[7] = 'приехал';
+array[9] = 'нужных вещей,'
+
+
+const result = `${array[3]} ${array[7]} ${array[0]} ${array[8]} ${array[11]} ${array[5]} ${array[9]} ${array[6]} ${array[4]} ${array[1]} ${array[12]} ${array[2]} ${array[10]}`;
+console.log(result);
+
+
+//Задание 4
+function showNames(firstName, lastName) {
+  const fullName = `${firstName} ${lastName}`;
+  console.log(fullName);
+}
+showNames(meObject.firstName, meObject.lastName);
+
+
+//Задание 5
+let num = 21; 
+while(num <= 67) {
+  if (num % 2 != 0) {
+    console.log(num);
+  }
+  num++;
+}
+
